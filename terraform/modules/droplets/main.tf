266be652ac56bacbb888576a9c739ff3d8b14edc
Kingsley Ijomah <git-example@codehance>
2024-04-08 18:32:00 +0900

    Adding manager

--- terraform/modules/droplets/main.tf
+++ terraform/modules/droplets/main.tf
@@ -19,17 +19,17 @@ variable "nodes" {
     size  : string
   }))
   default = {
-    "manager" = {
+    "manager1" = {
+      role  = "manager"
+      image = "ubuntu-23-10-x64"
+      size  = "s-2vcpu-4gb-120gb-intel"
+    },
+    "manager2" = {
       role  = "manager"
       image = "ubuntu-23-10-x64"
       size  = "s-2vcpu-4gb-120gb-intel"
     },
     "worker1" = {
-      role  = "worker"
-      image = "ubuntu-23-10-x64"
-      size  = "s-2vcpu-4gb-120gb-intel"
-    },
-    "worker2" = {
       role  = "worker"
       image = "ubuntu-23-10-x64"
       size  = "s-2vcpu-4gb-120gb-intel"

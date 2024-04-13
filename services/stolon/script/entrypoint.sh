9a17c6a323fc010e2840580dd413f71124406d9f
Kingsley Ijomah <git-example@codehance>
2024-04-13 08:49:21 +0300

    update

--- services/stolon/script/entrypoint.sh
+++ services/stolon/script/entrypoint.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-set -e
-
-# Default to run as keeper if not specified
-ROLE=${ROLE:-keeper}
 
 echo "Starting Stolon as a $ROLE..."
 
 case "$ROLE" in
-    "keeper")
-        exec stolon-keeper --data-dir /stolon-data --cluster-name ${STOLONCTL_CLUSTER_NAME} --store-backend ${STOLONCTL_STORE_BACKEND} --store-endpoints ${STOLONCTL_STORE_URL}
-        ;;
-    "sentinel")
-        exec stolon-sentinel --cluster-name ${STOLONCTL_CLUSTER_NAME} --store-backend ${STOLONCTL_STORE_BACKEND} --store-endpoints ${STOLONCTL_STORE_URL}
-        ;;
-    "proxy")
-        exec stolon-proxy --listen-address 0.0.0.0 --port 5432 --cluster-name ${STOLONCTL_CLUSTER_NAME} --store-backend ${STOLONCTL_STORE_BACKEND} --store-endpoints ${STOLONCTL_STORE_URL}
-        ;;
-    *)
-        echo "Unknown role: $ROLE"
-        exit 1
-        ;;
+  "keeper")
+    exec stolon-keeper --data-dir /stolon-data --cluster-name $STOLONCTL_CLUSTER_NAME --store-backend $STOLONCTL_STORE_BACKEND --store-endpoints $STOLONCTL_STORE_URL
+    ;;
+  "sentinel")
+    exec stolon-sentinel --cluster-name $STOLONCTL_CLUSTER_NAME --store-backend $STOLONCTL_STORE_BACKEND --store-endpoints $STOLONCTL_STORE_URL
+    ;;
+  "proxy")
+    exec stolon-proxy --cluster-name $STOLONCTL_CLUSTER_NAME --store-backend $STOLONCTL_STORE_BACKEND --store-endpoints $STOLONCTL_STORE_URL
+    ;;
+  *)
+    echo "Unknown role: $ROLE"
+    exit 1
+    ;;
 esac

d49fcd05d3eda2b4f46066ec38e5fec868577e4a
Kingsley Ijomah <git-example@codehance>
2024-04-13 19:02:05 +0300

    update

--- services/stolon/script/entrypoint.sh
+++ services/stolon/script/entrypoint.sh
@@ -2,15 +2,29 @@
 
 echo "Starting Stolon as a $ROLE..."
 
+# Fetch the IP address of the container
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+
 case "$ROLE" in
   "keeper")
-    exec stolon-keeper --data-dir /stolon-data --cluster-name $STOLONCTL_CLUSTER_NAME --store-backend $STOLONCTL_STORE_BACKEND --store-endpoints $STOLONCTL_STORE_URL
+    exec stolon-keeper \
+      --data-dir /stolon-data \
+      --cluster-name $STOLONCTL_CLUSTER_NAME \
+      --store-backend $STOLONCTL_STORE_BACKEND \
+      --store-endpoints $STOLONCTL_STORE_URL \
+      --pg-listen-address $IP_ADDRESS
     ;;
   "sentinel")
-    exec stolon-sentinel --cluster-name $STOLONCTL_CLUSTER_NAME --store-backend $STOLONCTL_STORE_BACKEND --store-endpoints $STOLONCTL_STORE_URL
+    exec stolon-sentinel \
+      --cluster-name $STOLONCTL_CLUSTER_NAME \
+      --store-backend $STOLONCTL_STORE_BACKEND \
+      --store-endpoints $STOLONCTL_STORE_URL
     ;;
   "proxy")
-    exec stolon-proxy --cluster-name $STOLONCTL_CLUSTER_NAME --store-backend $STOLONCTL_STORE_BACKEND --store-endpoints $STOLONCTL_STORE_URL
+    exec stolon-proxy \
+      --cluster-name $STOLONCTL_CLUSTER_NAME \
+      --store-backend $STOLONCTL_STORE_BACKEND \
+      --store-endpoints $STOLONCTL_STORE_URL
     ;;
   *)
     echo "Unknown role: $ROLE"

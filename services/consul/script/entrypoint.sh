8609cfcc09bd52f5960fd47f6f28f1a99e3b7fda
Kingsley Ijomah <git-example@codehance>
2024-04-20 07:07:30 -0500

    update

--- services/consul/script/entrypoint.sh
+++ services/consul/script/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Detect the private IP address
-IP_ADDRESS=$(hostname -I | awk '{print $1}')  
+# Detect the private IP address using ip command
+IP_ADDRESS=$(ip addr show eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
 
 # Update the Consul configuration file
 jq ".bind_addr = \"$IP_ADDRESS\" | .advertise_addr = \"$IP_ADDRESS\"" /consul/config/consul-config.json > /consul/config/temp.json && mv /consul/config/temp.json /consul/config/consul-config.json

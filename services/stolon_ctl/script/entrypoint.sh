e1681fe70b28dd3a8b52d1c77f22ddc10f8ab30b
Kingsley Ijomah <git-example@codehance>
2024-04-20 09:05:40 -0500

    update

--- services/stolon_ctl/script/entrypoint.sh
+++ services/stolon_ctl/script/entrypoint.sh
@@ -7,19 +7,12 @@ cluster_initialized() {
     return $?
 }
 
-# Wait for etcd to become available
-while ! nc -z etcd 2379; do
-    echo 'Waiting for etcd to become available...'
-    sleep 1
-done
-echo 'etcd is up and running!'
-
 # Initialize the cluster if it's not already initialized
 if ! cluster_initialized; then
     echo "Initializing Stolon cluster..."
-    # This command initializes the Stolon cluster configuration in the etcd database. It sets up the necessary 
-    # data structures in etcd under the specified cluster name "stolon-cluster", using etcd as the backend storage.
-    # This includes writing initial configuration and state data into etcd, which Stolon components will use for
+    # This command initializes the Stolon cluster configuration in the consul database. It sets up the necessary 
+    # data structures in consul under the specified cluster name "stolon-cluster", using consul as the backend storage.
+    # This includes writing initial configuration and state data into consul, which Stolon components will use for
     # managing the PostgreSQL cluster.
     stolonctl init --cluster-name=stolon-cluster --store-backend=consul --store-endpoints=http://consul:8500 --yes
 else

fa4e9771d493a855dae4a50d934ffaa7262f5110
Kingsley Ijomah <git-example@codehance>
2024-04-21 21:18:18 -0500

    update

--- services/stolon/script/entrypoint.sh
+++ services/stolon/script/entrypoint.sh
@@ -15,43 +15,6 @@ echo "Consul is ready."
 # Fetch the IP address of the container
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
-# # Initialize the database system and create users
-# if [ "$ROLE" = "keeper" ]; then
-#     # echo "Initializing PostgreSQL data directory..."
-#     # gosu postgres pg_ctl init -D "$PGDATA"
-#     # gosu postgres pg_ctl -D "$PGDATA" -o "-c listen_addresses='*'" start
-
-#     # # Wait until PostgreSQL is ready to accept connections
-#     # echo "Waiting for PostgreSQL to start..."
-#     # while ! pg_isready -q -h localhost -p 5432 -U postgres; do
-#     #     sleep 1
-#     #     echo "Waiting for PostgreSQL..."
-#     # done
-#     # echo "PostgreSQL is operational."
-
-#     # echo "PostgreSQL is running. Creating users..."
-#     # # Create the replication user
-#     # gosu postgres psql -c "CREATE USER $PG_REPL_USERNAME REPLICATION LOGIN ENCRYPTED PASSWORD '$PG_REPL_PASSWORD';"
-#     # # Create the application user
-#     # gosu postgres psql -c "CREATE USER $PG_APP_USER WITH ENCRYPTED PASSWORD '$PG_APP_PASSWORD';"
-
-#     # # Check if PG_APP_DB variable is provided and create the database
-#     # if [ ! -z "$PG_APP_DB" ]; then
-#     #     echo "Creating application database $PG_APP_DB..."
-#     #     gosu postgres psql -c "CREATE DATABASE $PG_APP_DB;"
-
-#     #     echo "Granting all privileges on database $PG_APP_DB to user $PG_APP_USER..."
-#     #     gosu postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $PG_APP_DB TO $PG_APP_USER;"
-
-#     #     echo "Setting up privileges for user $PG_APP_USER on $PG_APP_DB..."
-#     #     # Ensure the user can create tables in the public schema
-#     #     gosu postgres psql -d $PG_APP_DB -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $PG_APP_USER;"
-#     #     gosu postgres psql -d $PG_APP_DB -c "GRANT USAGE, CREATE ON SCHEMA public TO $PG_APP_USER;"
-#     # else
-#     #     echo "No application database specified. Skipping database creation."
-#     # fi
-# fi
-
 if [ "$ROLE" = "sentinel" ]; then
     # Verify registration with Consul
     while ! curl -s "http://$CONSUL_HOST:$CONSUL_PORT/v1/kv/stolon/cluster/$STOLONCTL_CLUSTER_NAME/keepers/info?keys" | grep -q "$KEEPER_ID"; do
@@ -74,9 +37,9 @@ case "$ROLE" in
       --pg-repl-password $PG_REPL_PASSWORD \
       --pg-su-username $PG_SU_USERNAME \
       --pg-su-password $PG_SU_PASSWORD \
-      --uid postgres1 \
-      --pg-bin-path /usr/lib/postgresql/16/bin \
-      --pg-port 5435
+      --uid $STKEEPER_UID \
+      --pg-bin-path $PG_BIN_PATH \
+      --pg-port $PG_PORT
     ;;
   "sentinel")
     exec stolon-sentinel \

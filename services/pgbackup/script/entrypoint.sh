224c22bb6857ac60a9ff9fd56ce28380839bcd94
Kingsley Ijomah <git-example@codehance>
2024-05-01 21:19:40 -0500

    update

--- services/pgbackup/script/entrypoint.sh
+++ services/pgbackup/script/entrypoint.sh
@@ -3,9 +3,9 @@
 # Setup cron job
 
 # Create a cron job that runs every 1 minutes
-echo "* * * * * PGPASSWORD=repl_password pg_basebackup -h proxy -p 5432 -U repluser -D \"/backups/\$(date +\%Y\%m\%d-\%H\%M\%S)\"  -Fp -X stream > /var/log/pg_basebackup.log 2>&1" > /etc/cron.d/pg_basebackup
+# echo "* * * * * PGPASSWORD=repl_password pg_basebackup -h proxy -p 5432 -U repluser -D \"/backups/\$(date +\%Y\%m\%d-\%H\%M\%S)\"  -Fp -X stream > /var/log/pg_basebackup.log 2>&1" > /etc/cron.d/pg_basebackup
 # Create a cron job that runs every day at 3:00 AM
-# echo "0 3 * * *  PGPASSWORD=repl_password pg_basebackup -h proxy -p 5432 -U repluser -D \"/backups/\$(date +\%Y\%m\%d-\%H\%M\%S)\" -Fp -X stream > /var/log/pg_basebackup.log 2>&1" > /etc/cron.d/pg_basebackup
+echo "0 3 * * *  PGPASSWORD=repl_password pg_basebackup -h proxy -p 5432 -U repluser -D \"/backups/\$(date +\%Y\%m\%d-\%H\%M\%S)\" -Fp -X stream > /var/log/pg_basebackup.log 2>&1" > /etc/cron.d/pg_basebackup
 
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/pg_basebackup
